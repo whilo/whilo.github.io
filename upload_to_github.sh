cf52590c3b8d0a1adf30bf8a3263b2341229aa85
Christian Weilbach <ch_weil@topiq.es>
2022-11-06 21:10:23 -0800

    Upgrade to logseq.

--- upload_to_github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-jekyll build
-rm -rf ../whilo.github.io/*
-cp -Rpvf _site/* ../whilo.github.io/
-cd ../whilo.github.io
-git add .
-git commit -m "Update blog."
-git push origin master
